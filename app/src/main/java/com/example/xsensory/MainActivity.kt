4ca7a34a4d1a78f794ece3a5fdb8d379e4385818
K2E7 <k2e7@outlook.com>
2023-12-02 21:50:35 +0530

    Hello Again
    XSensory_Simplified can now save the text inputted in a file when send is pressed.
    
    I have tried to integrate Nearby API with the code. Now a few essential permissions are required which need to be added at runtime.

--- app/src/main/java/com/example/xsensory/MainActivity.kt
+++ app/src/main/java/com/example/xsensory/MainActivity.kt
@@ -17,17 +17,59 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.xsensory.ui.theme.XSensory_CanaryTheme
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.AdvertisingOptions
+import com.google.android.gms.nearby.connection.ConnectionInfo
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
+import com.google.android.gms.nearby.connection.ConnectionResolution
+import com.google.android.gms.nearby.connection.ConnectionsClient
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo
+import com.google.android.gms.nearby.connection.DiscoveryOptions
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback
+import com.google.android.gms.nearby.connection.Strategy
 import java.io.BufferedWriter
 import java.io.File
-import java.io.FileOutputStream
 import java.io.FileWriter
 import java.io.IOException
 
 class MainActivity : ComponentActivity() {
+
+    private val SERVICE_ID = "XSensory-TestRelease"
+    private lateinit var connectionsClient : ConnectionsClient
     private var messageText: String = ""
+
+    private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+            // Handle the connection initiation from the remote device
+            // You can accept or reject the connection here
+        }
+
+        override fun onConnectionResult(endpointId: String, resolution: ConnectionResolution) {
+            // Handle the connection result, whether it's successful or not
+        }
+
+        override fun onDisconnected(endpointId: String) {
+            // Handle the disconnection of the remote device
+        }
+    }
+
+    private val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
+        override fun onEndpointFound(endpointId: String, discoveredEndpointInfo: DiscoveredEndpointInfo) {
+            // Handle the discovery of a nearby endpoint
+        }
+
+        override fun onEndpointLost(endpointId: String) {
+            // Handle the loss of a nearby endpoint
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Initialize Nearby Connections
+        connectionsClient = Nearby.getConnectionsClient(this)
+
+        startAdvertising()
         setContent {
             XSensory_CanaryTheme {
                 Column(
@@ -44,6 +86,7 @@ class MainActivity : ComponentActivity() {
                     }
                     SendButton {
                         createTextFileInSubfolder("XSensory")
+                        startDiscovery()
                     }
                     FilesButton {
 
@@ -85,4 +128,42 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    private fun startAdvertising() {
+        val advertisingOptions = AdvertisingOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build()
+
+        connectionsClient.startAdvertising(
+            /* p0 = */ "MyDevice", // Set a name for your device
+            /* p1 = */ SERVICE_ID,
+            /* p2 = */ connectionLifecycleCallback,
+            /* p3 = */ advertisingOptions
+        )
+            .addOnSuccessListener {
+                // Advertising started successfully
+                Toast.makeText(this, "Advertising started", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                // Advertising failed
+                Toast.makeText(this, "Advertising failed: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun startDiscovery() {
+        // Discovery options (use the same strategy as in advertising)
+        val discoveryOptions = DiscoveryOptions.Builder().setStrategy(Strategy.P2P_CLUSTER).build()
+
+        // Start discovery
+        connectionsClient.startDiscovery(
+            /* p0 = */ SERVICE_ID,
+            /* p1 = */ endpointDiscoveryCallback,
+            /* p2 = */ discoveryOptions
+        )
+            .addOnSuccessListener {
+                // Discovery started successfully
+                Toast.makeText(this, "Discovery started", Toast.LENGTH_SHORT).show()
+            }
+            .addOnFailureListener {
+                // Discovery failed
+                Toast.makeText(this, "Discovery failed: ${it.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
 }
